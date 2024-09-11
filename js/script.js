b3884546ca9c0263e0b1a8aa2526275b5fedfcb7
Lucas Teles <teles5996@gmail.com>
2024-09-10 21:17:45 -0300

    fatorial e fibonacci adicionado

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function avermelhar() {
         if (elemento.id === 'paragrafo') {
             document.getElementById('paragrafo').style.color = 'red';
             elemento.id = 'paragrafoTrocado1';
-            elemento.style.fontSize = '2em';
+            elemento.style.fontFamily = 'Arial';
         } else if (elemento.id === 'paragrafoTrocado1') {
             document.getElementById('paragrafoTrocado1').style.color = 'green';
             elemento.id = 'paragrafoTrocado2';
@@ -39,7 +39,7 @@ function avermelhar() {
             document.getElementById('paragrafoTrocado2').style.color = 'blue';
             elemento.id = 'paragrafo';
         }
-    }else {
+    } else {
         alert("falhou cores")
     }
 }
@@ -56,7 +56,7 @@ function sominha() {
     document.getElementById('sominha').innerHTML = `Soma: ${soma}`;
 }
 
-function subtracao(){
+function subtracao() {
     let input1 = document.getElementById('input1').value;
     let input2 = document.getElementById('input2').value;
 
@@ -70,4 +70,65 @@ function subtracao(){
 
 function consoleT() {
     console.log("teste");
+}
+
+function fibonacci() {
+    let input1 = document.getElementById('inputFibonacci').value;
+
+    let numeroAtual = 1;
+    let numeroAnterior = 1;
+    let proximoNumero = 0;
+
+    const resultadoElement = document.getElementById("resultadoFibonacci");
+    resultadoElement.textContent = '';
+
+    const termo1Element = document.createElement('div');
+    termo1Element.textContent = `Termo 1 = ${numeroAtual}`;
+    resultadoElement.appendChild(termo1Element);
+
+    const termo2Element = document.createElement('div');
+    termo2Element.textContent = `Termo 2 = ${numeroAnterior}`;
+    resultadoElement.appendChild(termo2Element);
+
+    for (let contadora = 3; contadora <= input1; contadora++) {
+        proximoNumero = numeroAnterior + numeroAtual;
+        const termoElement = document.createElement('div')
+        termoElement.textContent = `Termo ${contadora} = ${proximoNumero}`;
+        resultadoElement.appendChild(termoElement);
+        numeroAnterior = numeroAtual;
+        numeroAtual = proximoNumero;
+    }
+
+    // Forma mais simplificada: 
+
+    // const resultadoElement = document.getElementById("resultadoFibonacci");
+    // resultadoElement.innerHTML = ''; // clear the element
+
+    // resultadoElement.innerHTML += `Termo 1 = ${numeroAtual}<br>`;
+    // resultadoElement.innerHTML += `Termo 2 = ${numeroAnterior}<br>`;
+
+    // for (let contadora = 3; contadora <= input1; contadora++) {
+    //     proximoNumero = numeroAnterior + numeroAtual;
+    //     resultadoElement.innerHTML += `Termo ${contadora} = ${proximoNumero}<br>`;
+    //     numeroAnterior = numeroAtual;
+    //     numeroAtual = proximoNumero;
+    // }
+
+
+}
+
+function fatorial(){
+    let input1 = document.getElementById('inputFatorial').value;
+
+    let fatorial = 1
+
+    const resultadoElement = document.getElementById("resultadoFatorial");
+    resultadoElement.textContent = '';
+
+    for (let contadora = 1; contadora <= input1; contadora++) {
+        fatorial = fatorial * contadora;
+        const termoElement = document.createElement('div');
+        termoElement.textContent = `Fatorial de ${contadora} = ${fatorial}`;i
+        resultadoElement.appendChild(termoElement);
+    }
 }
